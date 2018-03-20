657bdf54ef17e380c6275b5864a2ff6ce626e263
enox <enoxx.root@protonmail.ch>
2018-03-21 00:55:22 +0300

    accuracy improved, pipe-though-vim technique used

--- market_parse.sh
+++ market_parse.sh
@@ -9,4 +9,4 @@ then
 fi
 
 echo -e "name\tmarket"
-links -dump "$uri" -html-assume-codepage UTF-8 | tr -d '\n' | sed 's/ ₽/ ₽\n/g;s/ОтложитьОтложено/\nОтложитьОтложено/g;s/₽/₽\n/g' | sed 's/^ *//g;s/ОтложитьОтложено   СравнитьВ сравнении   //g; s/   /\t/g' | grep ₽ | sed '/Показать еще/d' | awk -F'\t' 'OFS="\t"{print $2,$3}' | sed '/^\s*$/d' | sed '/отзыв/d' | grep ₽ | sed 's/\t\([0-9]*\) \([0-9]*\)/\t\1\2/g;s/ ₽//'
+links -dump "$uri" -html-assume-codepage UTF-8 | grep -v '*' | grep -vE 'СравнитьВ сравнении|Добавить в отложенные|Новинка|отзыв|предлож|доставка|самовывоз|Гарантия производителя|Посмотреть на карте|Пожаловаться|Защищённая покупка|Акция|\[IMG\]' | grep -vE "^[0-9. ]*$" | sed 's/\t/ /g' | nvim - -esn -c 'set nowrapscan | exe "%norm gg/ОтложитьОтложено\ndddd/₽\nPA\t\033gJ"' -c 'w!/dev/fd/3|q!' 3>&1 >/dev/null 2>&1 | grep -E '[0-9] ₽$' | sed 's/ \+/ /g; s/^ //g; s/ *\t */\t/g' | sed 's/\t\([0-9]*\) \([0-9]*\)/\t\1\2/g;s/ *₽//'
